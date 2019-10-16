5b174b13c6024140f48246b1102fcb6e1f56a02d
Jeffrey Paulraj <jeffrey.paulraj@gmail.com>
2019-10-15 20:13:50 -0400

    First release

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.text.method.ScrollingMovementMethod;
+
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
     String answerString;
     ArrayList<String> operationList;
-    ArrayList<Double> numberList;
-    ArrayList<String> totalList;
     ArrayList<Double> condensedNumberList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,204 +59,158 @@ public class MainActivity extends AppCompatActivity {
         buttonClear = findViewById(R.id.id_clearButton);
 
         answerField = findViewById(R.id.id_answerField);
+        answerField.setMovementMethod(new ScrollingMovementMethod());
 
         answerString = "";
         operationList = new ArrayList<>();
-        numberList = new ArrayList<>();
         condensedNumberList = new ArrayList<>();
-        totalList = new ArrayList<>();
 
         buttonOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(1.0);
                 operationList.add(null);
                 answerString+="1";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(2.0);
                 operationList.add(null);
-                answerString+="2";
+                answerString += "2";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(3.0);
                 operationList.add(null);
                 answerString+="3";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(4.0);
                 operationList.add(null);
                 answerString+="4";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonFive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(5.0);
                 operationList.add(null);
                 answerString+="5";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonSix.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(6.0);
                 operationList.add(null);
                 answerString+="6";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonSeven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(7.0);
                 operationList.add(null);
                 answerString+="7";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonEight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(8.0);
                 operationList.add(null);
                 answerString+="8";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonNine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(9.0);
                 operationList.add(null);
                 answerString+="9";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberList.add(0.0);
                 operationList.add(null);
                 answerString+="0";
                 answerField.setText(answerString);
-                totalList.add("Num");
-
             }
         });
         buttonAddition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 operationList.add("+");
-                numberList.add(null);
                 answerString+="+";
                 answerField.setText(answerString);
-                totalList.add("Operation");
-
             }
         });
         buttonSubtraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 operationList.add("-");
-                numberList.add(null);
                 answerString+="-";
                 answerField.setText(answerString);
-                totalList.add("Operation");
-
             }
         });
         buttonMultiplication.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 operationList.add("*");
-                numberList.add(null);
                 answerString+="*";
                 answerField.setText(answerString);
-                totalList.add("Operation");
-
             }
         });
         buttonDivision.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 operationList.add("/");
-                numberList.add(null);
                 answerString+="/";
                 answerField.setText(answerString);
-                totalList.add("Operation");
-
             }
         });
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 answerString = "";
-                answerField.setText(answerString);
+                answerField.setText("0");
             }
         });
         buttonEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 double answer = getSolution();
-                System.out.println(answer);
+                answerString = "" + answer;
                 answerField.setText("" + answer);
             }
         });
     }
 
     public double getSolution(){
-       double Solution = 0;
-        // if(operationList.size() == 0){
-         //   return
-        //}
-        double currentNumber = 0.0;
-        if(numberList.size() == 0){
+        double Solution = 0;
+
+        if(condensedNumberList.size() == 0){
             return 0;
         }
-        for(int i = 1; i < numberList.size(); i++){
-            if(numberList.get(i-1) != null && numberList.get(i) != null){
-                currentNumber = 10*numberList.get(i-1) + numberList.get(i);
-            }
-            else if (numberList.get(i-1) != null && numberList.get(i) == null){
-                condensedNumberList.add(currentNumber);
-                    currentNumber = numberList.get(i+1);
-            }
+        System.out.println("String: " + answerString);
+
+        StringTokenizer answerTokenizer = new StringTokenizer(answerString, "+-*/");
+        while(answerTokenizer.hasMoreTokens()){
+            String currentNumString = answerTokenizer.nextToken();
+            System.out.println(currentNumString);
+            double currentNum = Double.parseDouble(currentNumString);
+            condensedNumberList.add(currentNum);
         }
 
-        for(int i = 0; i < operationList.size(); i++){
+        for(int i = operationList.size() - 1; i >= 0; i--){
             if(operationList.get(i) == null){
                 operationList.remove(i);
             }
@@ -278,6 +233,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+        condensedNumberList = new ArrayList<>();
+        operationList = new ArrayList<>();
         return Solution;
     }
 }
