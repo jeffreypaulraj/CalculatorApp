ae12a643fadf2026dd6db1cabfdca037cdd90ebe
Jeffrey Paulraj <jeffrey.paulraj@gmail.com>
2019-10-16 20:26:25 -0400

    Programming Landscape Mode Buttons
    All Landscape Mode Buttons are now fully operational

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,28 @@ public class MainActivity extends AppCompatActivity {
     Button buttonDivision;
     Button buttonEqual;
 
+    Button buttonPi;
+    Button buttonE;
+    Button buttonRand;
+    Button buttonDecimal;
+    Button buttonSin;
+    Button buttonCos;
+    Button buttonTan;
+    Button buttonPlusMinus;
+    Button buttonSquare;
+    Button buttonCube;
+    Button buttonSquareRoot;
+    Button buttonCubeRoot;
+    Button buttonEPowerX;
+    Button buttonTenPowerX;
+    Button buttonLn;
+    Button buttonLog;
+    Button buttonSinH;
+    Button buttonCosH;
+    Button buttonTanH;
+    Button buttonDegreeToggle;
     Button buttonClear;
+    boolean degree;
 
     TextView answerField;
 
@@ -42,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        int orientation = getResources().getConfiguration().orientation;
         buttonOne = findViewById(R.id.id_oneButton);
         buttonTwo = findViewById(R.id.id_twoButton);
         buttonThree = findViewById(R.id.id_threeButton);
@@ -60,6 +82,219 @@ public class MainActivity extends AppCompatActivity {
         buttonEqual = findViewById(R.id.id_equalButton);
         buttonClear = findViewById(R.id.id_clearButton);
 
+        degree = false;
+        if (orientation == 2) {
+            buttonPi = findViewById(R.id.id_piButton);
+            buttonE = findViewById(R.id.id_eButton);
+            buttonRand = findViewById(R.id.id_randButton);
+            buttonDecimal = findViewById(R.id.id_decimalButton);
+            buttonSin = findViewById(R.id.id_sinButton);
+            buttonCos = findViewById(R.id.id_cosButton);
+            buttonTan = findViewById(R.id.id_tanButton);
+            buttonPlusMinus = findViewById(R.id.id_plusMinusButton);
+            buttonSquare = findViewById(R.id.id_squareButton);
+            buttonCube = findViewById(R.id.id_cubeButton);
+            buttonSquareRoot = findViewById(R.id.id_sqrtButton);
+            buttonCubeRoot = findViewById(R.id.id_cubeRootButton);
+            buttonEPowerX = findViewById(R.id.id_ePowerXButton);
+            buttonTenPowerX = findViewById(R.id.id_tenPowerXButton);
+            buttonLn = findViewById(R.id.id_lnButton);
+            buttonLog = findViewById(R.id.id_logButton);
+            buttonSinH = findViewById(R.id.id_sinhButton);
+            buttonCosH = findViewById(R.id.id_coshButton);
+            buttonTanH = findViewById(R.id.id_tanhButton);
+            buttonDegreeToggle = findViewById(R.id.id_degreeButton);
+
+
+            buttonDecimal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    operationList.add(null);
+                    answerString += ".";
+                    answerField.setText(answerString);
+                }
+            });
+
+            buttonRand.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    operationList = new ArrayList<>();
+                    answerString = "" + Math.random();
+                    answerField.setText(answerString);
+                }
+            });
+            buttonE.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    operationList = new ArrayList<>();
+                    answerString = "" + Math.E;
+                    answerField.setText(answerString);
+                }
+            });
+
+            buttonPi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    operationList = new ArrayList<>();
+                    answerString = "" + Math.PI;
+                    answerField.setText(answerString);
+                }
+            });
+
+            buttonPlusMinus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(answerString.length() == 0){
+                        answerString = "";
+                    }
+                    if(answerString.length() == 1 && answerString.charAt(0) == '0'){
+                        answerString = "0";
+                    }
+                    else if(answerString.charAt(0)=='-'){
+                        answerString = answerString.substring(1);
+                    }
+                    else {
+                        answerString = "-" + answerString;
+                    }
+                    answerField.setText(answerString);
+                }
+            });
+
+            buttonSin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(!degree){
+                        checkAnswerError(Math.sin(getSolution()));
+                    }
+                    else{
+                        checkAnswerError(Math.sin(Math.toRadians(getSolution())));
+                    }
+                }
+            });
+
+            buttonCos.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(!degree){
+                        checkAnswerError(Math.cos(getSolution()));
+                    }
+                    else{
+                        checkAnswerError(Math.cos(Math.toRadians(getSolution())));
+                    }
+                }
+            });
+
+            buttonTan.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(!degree){
+                        checkAnswerError(Math.tan(getSolution()));
+                    }
+                    else{
+                        checkAnswerError(Math.tan(Math.toRadians(getSolution())));
+                    }
+                }
+            });
+
+            buttonSquare.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    double answer = getSolution();
+                    checkAnswerError(answer);
+                    if(!error){
+                        checkAnswerError(answer*answer);
+                    }
+                }
+            });
+
+            buttonCube.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    double answer = getSolution();
+                    checkAnswerError(answer);
+                    if(!error){
+                        checkAnswerError(answer*answer*answer);
+                    }
+                }
+            });
+
+            buttonSquareRoot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.sqrt(getSolution()));
+                }
+            });
+
+            buttonCubeRoot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.cbrt(getSolution()));
+                }
+            });
+
+            buttonLn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.log(getSolution()));
+                }
+            });
+
+            buttonLog.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.log10(getSolution()));
+                }
+            });
+
+            buttonEPowerX.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.exp(getSolution()));
+                }
+            });
+
+            buttonTenPowerX.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.pow(10,getSolution()));
+                }
+            });
+
+            buttonSinH.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.sinh(getSolution()));
+                }
+            });
+
+            buttonCosH.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.cosh(getSolution()));
+                }
+            });
+
+            buttonTanH.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkAnswerError(Math.tanh(getSolution()));
+                }
+            });
+
+            buttonDegreeToggle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    degree = !degree;
+
+                    if(degree){
+                        buttonDegreeToggle.setText("DEG");
+                    }
+                    else{
+                        buttonDegreeToggle.setText("RAD");
+                    }
+                }
+            });
+        }
         answerField = findViewById(R.id.id_answerField);
         answerField.setMovementMethod(new ScrollingMovementMethod());
 
@@ -140,6 +375,7 @@ public class MainActivity extends AppCompatActivity {
                 answerField.setText(answerString);
             }
         });
+
         buttonZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -190,14 +426,7 @@ public class MainActivity extends AppCompatActivity {
         buttonEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double answer = getSolution();
-                answerString = "" + answer;
-                if(error == true){
-                    answerField.setText("Error");
-                }
-                else {
-                    answerField.setText("" + answer);
-                }
+                checkAnswerError(getSolution());
             }
         });
     }
@@ -217,6 +446,9 @@ public class MainActivity extends AppCompatActivity {
         }
         if(condensedNumberList.size() == 0){
             return 0;
+        }
+        if(answerString.charAt(0) == '-'){
+            condensedNumberList.set(0,condensedNumberList.get(0)*-1);
         }
         for(int i = operationList.size() - 1; i >= 0; i--){
             if(operationList.get(i) == null){
@@ -250,5 +482,16 @@ public class MainActivity extends AppCompatActivity {
         condensedNumberList = new ArrayList<>();
         operationList = new ArrayList<>();
         return Solution;
+    }
+
+    public void checkAnswerError(double answer){
+        answerString = "" + answer;
+        if(error){
+            answerString = "" + 0;
+            answerField.setText("Error");
+        }
+        else {
+            answerField.setText("" + answer);
+        }
     }
 }
